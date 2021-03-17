ec054a8af96b50d4a4f703f4913bb2dadc251577
Cloudbop <boppincolin@gmail.com>
2021-03-17 18:59:40 +0000

    added main-nav+logo+styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Hero from "../components/homepage/hero";
+import Layout from "../components/layout/layout";
 
 function HomePage() {
   return (
-    <Fragment>
+    <Layout>
       <Hero />
-    </Fragment>
+    </Layout>
   );
 }
 
