273c026699af317438a3065cc900e2a1ebebec58
Cloudbop <boppincolin@gmail.com>
2021-03-17 19:06:19 +0000

    rendered post-grid in featured post + Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FeaturedPosts from "../components/homepage/featured-posts";
 import Hero from "../components/homepage/hero";
 import Layout from "../components/layout/layout";
 
@@ -6,6 +7,7 @@ function HomePage() {
   return (
     <Layout>
       <Hero />
+      <FeaturedPosts />
     </Layout>
   );
 }
