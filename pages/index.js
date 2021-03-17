9112ed8af4ceeb957a287242d47c5d3c0577e2e9
Cloudbop <boppincolin@gmail.com>
2021-03-17 23:21:18 +0000

    added getStaticProps to post feeds

--- pages/index.js
+++ pages/index.js
@@ -2,47 +2,27 @@ import React from "react";
 import FeaturedPosts from "../components/homepage/featured-posts";
 import Hero from "../components/homepage/hero";
 import Layout from "../components/layout/layout";
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  }
-];
-function HomePage() {
+
+import { getFeaturedPosts } from "../lib/posts-util";
+//
+function HomePage(props) {
+  const { posts } = props;
   return (
     <Layout>
       <Hero />
-      <FeaturedPosts posts={[...DUMMY_POSTS]} />
+      <FeaturedPosts posts={posts} />
     </Layout>
   );
+}
+
+export async function getStaticProps(context) {
+  const featruedPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featruedPosts
+    }
+    // revalidate: null - never rerun this after build - content not likely to change
+  };
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,42 +1,18 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../lib/posts-util";
+function AllPostsPage(props) {
+  const { posts } = props;
+  return <AllPosts posts={posts} />;
+}
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  },
-  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  },
-  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  },
-  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
-    date: "2022-02-10"
-  }
-];
-
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export async function getStaticProps(context) {
+  const publishedPosts = getAllPosts();
+  return {
+    props: {
+      posts: publishedPosts
+    }
+    // revalidate: null - never rerun this after build - content not likely to change
+  };
 }
 
 export default AllPostsPage;
