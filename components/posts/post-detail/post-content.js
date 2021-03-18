74df8b0d773d77c0df7d50fbcb137324ccf50cff
Cloudbop <boppincolin@gmail.com>
2021-03-18 00:02:38 +0000

    pre-rendered dynamic posts

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,21 +2,17 @@ import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# This is a first post"
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const {
+    post: { slug, image, title, content }
+  } = props;
+  //
+  const imagePath = `/images/posts/${slug}/${image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={title} image={imagePath} />
+      <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,49 @@
 import React from "react";
 import PostContent from "../../components/posts/post-detail/post-content";
-function SinglePostPage() {
-  return <PostContent />;
+import { getPostsFiles, getPostData } from "../../lib/posts-util";
+function SinglePostPage(props) {
+  const { post } = props;
+
+  if (post === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  return <PostContent post={post} />;
+}
+
+export async function getStaticProps(context) {
+  // get the page slug
+  const { params } = context;
+  const { slug } = params;
+  //
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600 * 6 // every hour. rebuild - EG if revisions are made
+  };
+}
+
+//
+// - remember
+// need to use getStaticPath to pre-render SSR/SSG dynamic posts. - otherwise do not know what dynamic pages exist
+export async function getStaticPaths(context) {
+  const allPostsFilenames = getPostsFiles();
+
+  const slugs = allPostsFilenames.map(fileName =>
+    fileName.replace(/\.md$/, "")
+  );
+  return {
+    // SSG pre-render all these paths
+    paths: slugs.map(slug => ({ params: { slug: slug } })),
+    fallback: true
+  };
+  // nothing would be pre-renderedm but will try and return for everypage
+  // return {
+  //   paths: [],
+  //   fallback: true
+  // };
 }
 
 export default SinglePostPage;
