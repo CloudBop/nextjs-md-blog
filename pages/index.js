d85ddbe00d67dc422fb5c2ec4fd951f950d01648
Cloudbop <boppincolin@gmail.com>
2021-03-18 23:06:00 +0000

    added meta for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import FeaturedPosts from "../components/homepage/featured-posts";
 import Hero from "../components/homepage/hero";
+import Head from "next/head";
 
 import { getFeaturedPosts } from "../lib/posts-util";
 //
@@ -8,6 +9,13 @@ function HomePage(props) {
   const { posts } = props;
   return (
     <Fragment>
+      <Head>
+        <title>CloudBop' NextJS Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
